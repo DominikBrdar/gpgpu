c7e6a61fa1a5818f3aab5c6b9ddfaa3a7382f6d8
PajserDivizija <luka.pranjic@fer.hr>
2022-06-03 03:09:15 +0200

    Prva dva zadatka gotova

--- zad2.py
+++ zad2.py
@@ -10,7 +10,7 @@ if __name__=='__main__':
 	
 	# get info of avalible platform and gpu
 	platform = cl.get_platforms()
-	gpu_devices = platform[1].get_devices()	
+	gpu_devices = platform[0].get_devices()	
 	print("Platforms: " + str(platform))
 	print("OpenCL devices: " + str(gpu_devices))
 	
@@ -18,19 +18,19 @@ if __name__=='__main__':
 	queue = cl.CommandQueue(ctx)
 	
 	cores = 0
-	N = 10**7
+	N = 2**21
 	# Calculate pi in n cycles with opencl on specified number of cores. cores=0 => all cores
 	p = cl.Program(ctx, """
-		__kernel void calac_pi(__global float *result, const int n) {
+		__kernel void calc_pi(__global float *result, const int n) {
 	  		int gid = get_global_id(0);
 	  		float x = (((float)gid - 0.5) / (float)n);
 	  		result[gid] = 4.0 / (1.0 + x * x);
 	}""").build()
 	mf = cl.mem_flags
-	r = np.zeros(n, dtype=np.float32)
+	r = np.zeros(N, dtype=np.float32)
 	r_buf = cl.Buffer(ctx, mf.WRITE_ONLY, r.nbytes)
 	
-	p.calc_pi(queue, r.shape, None, r_buf, np.int32(N))
+	p.calc_pi(queue, r.shape, (128, ), r_buf, np.int32(N))
 	cl.enqueue_copy(queue, r, r_buf)
 	print("PI = " + str(r.sum()/N) + "\n n: " + str(N))
 	
